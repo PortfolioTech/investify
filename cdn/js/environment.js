c101abb7543176a3a3a39a7b0e9cd252f9c5962f
RenBelDev <renebelleisle@hotmail.com>
2024-05-09 21:05:07 -0400

    Deploy to prod v1.2

---
+++ cdn/js/environment.js
@@ -0,0 +1,31 @@
+export const dev_url = "DEV31620240503";
+
+export const prod_url = "../vues/investify.vue";
+
+export const settings_url = "https://portfoliotech.github.io/investify/cdn/html/settings.html";
+
+export function set_dev_url(url) {
+    localStorage.setItem(dev_url, url);
+    return "dev url set: " + url;
+}
+
+export function get_dev_url() {
+    return localStorage.getItem(dev_url);
+}
+
+export function delete_dev_url() {
+    localStorage.removeItem(dev_url);
+    return "dev url deleted";
+}
+
+export function is_dev_env(domain) {
+    return !domain.includes('github') || get_dev_url();
+}
+
+export function dev_env_enabled(domain) {
+    return domain.includes('github') && get_dev_url();
+}
+
+export function get_module_url(domain) {
+    return dev_env_enabled(domain) ? get_dev_url() : prod_url;
+}
--- cdn/js/settings_Bck.js
+++
@@ -1,6 +0,0 @@
-import * as env from './environment.js';
-
-window.setDevEnvironmentFlag = env.setDevEnvironmentFlag;
-window.alertDevEnvironmentFlag = env.alertDevEnvironmentFlag;
-window.removeDevEnvironmentFlag = env.removeDevEnvironmentFlag;
-window.SettingsUrl = env.SettingsUrl;
