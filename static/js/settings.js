171c8180b4b948b9a6297b098e56a3fcd72f680b
RenBelDev <renebelleisle@hotmail.com>
2024-06-11 21:26:47 -0400

    Deploy to prod v1.3

--- static/js/settings.js
+++ static/js/settings.js
@@ -1,19 +1,16 @@
+export const version_url = "VERSION110620240857";
 export const dev_url = "DEV31620240503";
-
 export const prod_url = "../vues/investify.vue";
-
 export const settings_page_url = "https://portfoliotech.github.io/investify/static/html/settings.html";
 
-export function set_dev_url(url) {
-    localStorage.setItem(dev_url, url);
+
+export function set_url(key_url, url) {
+    localStorage.setItem(key_url, url);
 }
 
-export function get_dev_url() {
-    return localStorage.getItem(dev_url);
-}
-
-export function delete_dev_url() {
-    localStorage.removeItem(dev_url);
+export function get_url(key_url) {
+    let url = localStorage.getItem(key_url);
+    return url ? url : prod_url;
 }
 
 export function dev_mode(domain) {
